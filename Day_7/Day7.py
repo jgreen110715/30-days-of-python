d86d15fe3a6e7df6c44e91aeb756f00342a83f2a
jgreen110715 <68028768+jgreen110715@users.noreply.github.com>
2023-01-20 15:55:38 -0500

    adding Day7

---
+++ Day_7/Day7.py
@@ -0,0 +1,29 @@
+# Day 7
+import math
+
+# Sets
+    # Set is a collection of items. Let me take you back to your elementary or high school Mathematics lesson. The Mathematics definition of a set can be applied also in Python. Set is a collection of unordered and un-indexed distinct elements. In Python set is used to store unique items, and it is possible to find the union, intersection, difference, symmetric difference, subset, super set and disjoint set among sets.
+
+# Creating a Set
+    # We use curly brackets, {} to creat a set or the set() built-in functions    
+    
+    # Creating an empty set
+        # Syntax
+            # st = {}
+            # st = set()
+
+    # Creating a set with initial items
+        # Syntax
+            # st = {'item1', 'item2', 'item3', 'item4'}
+
+fruits = {'banana', 'orange', 'mango', 'lemon'}
+
+# Getting Set's length
+    # We use the len() method to find the length of a set
+        # Syntax:
+            # st =  {'item1', 'item2', 'item3', 'item4'}
+            # len(st)
+
+fruits =  {'banana', 'orange', 'mango', 'lemon'}
+print(len(fruits))
+
---
+++ Day_7/Exercises_Day7.py
@@ -0,0 +1,5 @@
+# Day 7: 30 Days of python programming
+import math
+import statistics
+
+# Exercises: Day 7 Sets
