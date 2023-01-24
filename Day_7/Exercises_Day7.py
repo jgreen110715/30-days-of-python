75a2cef2ed121c53135e4fd02791583ea651e9c1
jgreen110715 <68028768+jgreen110715@users.noreply.github.com>
2023-01-24 11:44:06 -0500

    Completed

--- Day_7/Exercises_Day7.py
+++ Day_7/Exercises_Day7.py
@@ -39,28 +39,66 @@ print(it_companies)
 
 # 1. Join A and B
 
-
+A.update(B)
+print(A)
 
 # 2. Find A intersection B
 
+A = {19, 22, 24, 20, 25, 26}
+B = {19, 22, 20, 25, 26, 24, 28, 27}
+int = A.intersection(B)
+print(int)
 
 
 # 3. Is A subset of B
 
-
+print(A.issubset(B))
 
 # 4. Are A and B disjoint sets
 
-
+print(A.isdisjoint(B))
 
 # 5. Join A with B and B with A
 
-
+C = A.union(B)
+D = B.union(A)
+print(C)
+print(D)
 
 # 6. What is the symmetric difference between A and B
 
-
+print(A.symmetric_difference(B))
 
 # 7. Delete the sets completely
+
+del A
+del B
+
+# Exercises: Level 3
+
+# 1. Convert the ages to a set and compare the length of the list and the set, which one is bigger?
+
+age_st = set(age)
+print(len(age))
+print(len(age_st))
+
+if len(age) > len(age_st):
+    print('The List of Ages is longer than the Set of Ages')
+else:
+    print('The Set of Ages is longer than the List of Ages')
+
+
+#2. Explain the difference between the following data types: string, list, tuple and set
+
+# A String: is any length of characters placed between '' or ""
+
+# A List: is a muteable group, that can of different datatypes, where [] are used
+
+# A Tuple: A tuple is a collection of different data types which is ordered and unchangeable. Uses ()
+
+# A Set: Set is a collection of unordered and un-indexed distinct elements. In Python set is used to store unique items. Uses {}
+
+
+# 3. I am a teacher and I love to inspire and teach people. How many unique words have been used in the sentence? Use the split methods and set to get the unique words.
 
 
