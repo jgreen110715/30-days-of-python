b0253115fbf71ddca734e70be78e53a1a6759dc9
jgreen110715 <68028768+jgreen110715@users.noreply.github.com>
2023-02-03 13:42:48 -0500

    notes and level 1 done

--- Day_9/Day9.py
+++ Day_9/Day9.py
@@ -1,2 +1,115 @@
 # Day 7
 import math
+
+# 30 Days Of Python: Day 9 - Conditionals
+
+# Conditionals
+    # By default, statements in Python script are executed sequentially from top to bottom. If the processing logic require so, the sequential flow of execution can be altered in two ways:
+        # Conditional execution: a block of one or more statements will be executed if a certain expression is true
+        # Repetitive execution: a block of one or more statements will be repetitively executed as long as a certain expression is true. In this section, we will cover if, else, elif statements. The comparison and logical operators we learned in previous sections will be useful here.
+
+    # If Condition
+        # In python and other programming languages, the key word 'if' is used to check if a condition is true and to execute a block of code. Remember the indentation after the colon
+
+    # Syntax:
+        # if condition:
+            # this part of code runs for truthy conditions
+
+# Example:
+
+a = 3
+if a > 0:
+    print('A is positive number')
+# A is a positive number
+
+# As you can see in the example above, 3 is greater than 0. The condition was true and the block code was executed. However, if the condition is false, we do not see the result. In order to see the result of the falsy condition, we should have another block, which is going to be else.
+
+# If Else Condition
+    # If condition is true, the first block will be executed, if not, the else condition will run.
+        # Syntax:
+            # if condition:
+                # this part of code runs for truthy conditions
+            # else:
+                # this part of code runs for falsy conditions
+
+a = 3
+if a < 0:
+    print('A is negative number')
+else:
+    print('A is positive number')
+# A is positive number
+
+# The condition above proves false, therefore the else block was executed.
+
+# If Elif Else Condition
+    # In our daily life, we make decisions on a daily basis. We make decisions not by checking one or two conditions but multiple. As similar to life, programming is also full of conditions. We use elif when we have multiple conditions.
+        # Syntax:
+            # if condition:
+                # code
+            # elif condition:
+                # code
+            # else:
+                # code
+
+a = 0
+if a > 0:
+    print('A is positive number')
+elif a < 0:
+    print('A is negative number')
+else:
+    print('A is zero')
+    
+# Short Hand
+    # Syntax:
+        # code if condition else code
+
+a = 3
+print('A is positive number') if a > 0 else print('A is negative number')
+
+# Nested Conditions
+    # Conditions can be nested
+        # Syntax:
+            # if condition:
+                # code
+                # if condition:
+                    # code
+
+a = 0
+if a > 0:
+    if a % 2 == 0:
+        print('A is positive and even integer')
+    else:
+        print('A is a positive number')
+elif a == 0:
+    print('A is zero')
+else:
+    print('A is negative number')
+
+# We can avoid writing nested conditions by using the logical operator and.
+
+# If Condition with Logical Operators
+    # Syntax:
+        # if condition and condition:
+            # code
+
+a = 0
+if a > 0 and a % 2 == 0:
+    print('A is an even and positive integer')
+elif a > 0 and a % 2 != 0:
+    print('A is a positive number')
+elif a == 0:
+    print('A is zero')
+else:
+    print('A is negative number')
+
+# If and Or Logical Operators
+    # Syntax:
+        # if condition or condition:
+            # code
+
+user = 'Jon'
+access_level = 3
+if user == 'admin' or access_level >= 4:
+    print('Access granted')
+else:
+    print('Access denied')
--- Day_9/Exercises_Day9.py
+++ Day_9/Exercises_Day9.py
@@ -2,4 +2,49 @@
 import math
 import statistics
 
-# Exercises: Day 9 Dictionaries
+# Exercises: Day 9 - Conditionals
+# Level 1
+
+#1. Get user input using input(“Enter your age: ”). If user is 18 or older, give feedback: You are old enough to drive. If below 18 give feedback to wait for the missing amount of years. 
+    # Output:
+        # Enter your age: 30
+        # You are old enough to learn to drive.
+    # Output:
+        # Enter your age: 15
+        # You need 3 more years to learn to drive.
+
+age = int(input('Enter your age: '))
+if age >= 18:
+    print('You are old enough to drive.')
+else:
+    print('You need ' + str(18-age) + ' more years to learn to drive.')
+
+# 2. Compare the values of my_age and your_age using if … else. Who is older (me or you)? Use input(“Enter your age: ”) to get the age as input. You can use a nested condition to print 'year' for 1 year difference in age, 'years' for bigger differences, and a custom text if my_age = your_age. 
+    # Output:
+        # Enter your age: 30
+        # You are 5 years older than me.
+
+print('who is older me or you?')
+my_age = 34
+your_age = int(input('Enter your age: '))
+if my_age > your_age:
+    print('You are', my_age - your_age , 'years younger than me.')
+elif my_age < your_age:
+    print('You are', your_age - my_age,'years older than me.')
+else:
+    print('We are the same age.')
+
+# 3. Get two numbers from the user using input prompt. If a is greater than b return a is greater than b, if a is less b return a is smaller than b, else a is equal to b.
+    # Output:
+        # Enter number one: 4
+        # Enter number two: 3
+        # 4 is greater than 3
+
+a = int(input('Enter number one: '))
+b = int(input('Enter number two: '))
+if a > b:
+    print(a, 'is greater than', b)
+elif a < b:
+    print(a, 'is smaller than', b)
+else:
+    print(a, 'is equal to', b)
